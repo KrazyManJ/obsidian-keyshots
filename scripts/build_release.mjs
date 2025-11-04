31dd4c458a62a3a5d8c8533dd93fe1dc0897b2e1
KrazyManJ <KrazyManJ@gmail.com>
2025-11-05 00:06:19 +0100

    Updated build_release script

--- scripts/build_release.mjs
+++ scripts/build_release.mjs
@@ -1,32 +1,58 @@
-import JSZip from "jszip"
-import {readFileSync, unlinkSync, createWriteStream, mkdirSync, existsSync, copyFileSync, readdirSync} from "fs";
-import {join, basename, dirname} from "path"
-import { fileURLToPath } from 'url';
+import JSZip from "jszip";
+import {
+    readFileSync,
+    unlinkSync,
+    createWriteStream,
+    mkdirSync,
+    existsSync,
+    copyFileSync,
+    readdirSync,
+} from "fs";
+import { join, basename, dirname } from "path";
+import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const root = join(__dirname,"..")
+const root = join(__dirname, "..");
 
-const releaseFolder = join(root,"release")
-const manifestPath = join(root, "manifest.json")
-const mainPath = join(root, "main.js")
-const stylePath = join(root, "styles.css")
+const releaseFolder = join(root, "release");
+const manifestPath = join(root, "manifest.json");
+const mainPath = join(root, "main.js");
+const stylePath = join(root, "styles.css");
 
-if (!existsSync(releaseFolder)) mkdirSync(releaseFolder)
+if (!existsSync(releaseFolder)) mkdirSync(releaseFolder);
 
-readdirSync(releaseFolder).forEach( (file) => {  
+readdirSync(releaseFolder).forEach((file) => {
     if (file.endsWith(".zip")) unlinkSync(join(releaseFolder, file));
 });
 
 const release_zip = new JSZip();
 
-const manifest = readFileSync(manifestPath)
-const version = JSON.parse(manifest)["version"]
-release_zip.file("obsidian-keyshots/manifest.json",manifest)
-release_zip.file("obsidian-keyshots/main.js",readFileSync(mainPath))
-release_zip.file("obsidian-keyshots/styles.css",readFileSync(stylePath))
+const manifestFile = readFileSync(manifestPath);
+const manifest = JSON.parse(manifestFile);
 
-release_zip.generateNodeStream({ type: 'nodebuffer', streamFiles: true })
-    .pipe(createWriteStream(join(__dirname, "..", "release", `obsidian-keyshots-${version}.zip`)));
+const pluginName = manifest["id"];
+const pluginVersion = manifest["version"];
 
-[mainPath,manifestPath,stylePath].forEach(f => copyFileSync(f,join(releaseFolder+`/${basename(f)}`)));
+release_zip.file(`${pluginName}/manifest.json`, manifestFile);
+release_zip.file(`${pluginName}/main.js`, readFileSync(mainPath));
+release_zip.file(`${pluginName}/styles.css`, readFileSync(stylePath));
+
+release_zip
+    .generateNodeStream({ type: "nodebuffer", streamFiles: true })
+    .pipe(
+        createWriteStream(
+            join(
+                __dirname,
+                "..",
+                "release",
+                `${pluginName}-${pluginVersion}.zip`
+            )
+        )
+    );
+
+const filesToCopy = [mainPath, manifestPath, stylePath];
+
+filesToCopy.forEach((f) =>
+    copyFileSync(f, join(releaseFolder + `/${basename(f)}`))
+);
